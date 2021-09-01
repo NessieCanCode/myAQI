224c9176b7df24acd1b965008120fb01aa5c22f5
Robert Romero <1013414+NessieCanCode@users.noreply.github.com>
2021-08-31 20:38:05 -0700

    Create lambda_function.py

---
+++ myAQI_IFTTT_PM25_Forecasts/lambda_function.py
@@ -0,0 +1,33 @@
+import json
+import requests
+
+
+def lambda_handler(event, context):
+#Observed AQI category number:
+#1 Good
+#2 Moderate
+#3 Unhealthy for Sensitive Groups
+#4 Unhealthy
+#5 Very Unhealthy
+#6 Hazardous
+#7 Unavailable
+    #API_INPUT
+    users_zip = event['queryStringParameters']['users_zip']
+    url = "https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=" + users_zip + "&distance=25&API_KEY={API KEY}"
+    response = requests.get(url)
+    data = response.json()
+    aqi_type = data[0]['ParameterName']
+    aqi_value = data[0]['AQI']
+    aqi_category = data[0]['Category']['Name']
+    
+    api_response = {}
+    api_response['value1'] = '{0}'.format(aqi_type)
+    api_response['value2'] = '{0}'.format(aqi_value)
+    api_response['value3'] = '{0}'.format(aqi_category)
+    response_object = {}
+    response_object['statusCode'] = 200
+    response_object['headers'] = {}
+    response_object['headers']['Content-Type'] = 'application/json'
+    response_object['body'] = json.dumps(api_response)
+    
+    return response_object
